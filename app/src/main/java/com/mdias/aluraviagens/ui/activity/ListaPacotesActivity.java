d19dda43e91f8ee36ac26d88d2bd61dc9b46db71
Marcos Dias <marcos.a.melo.dias@gmail.com>
2018-09-09 03:13:06 -0300

    Introduction of Constraint Layouts

---
+++ app/src/main/java/com/mdias/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,15 @@
+package com.mdias.aluraviagens.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mdias.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
