c65679634c29f20bdb270b95d8ca21155b2bf5f1
Marcos Dias <marcos.a.melo.dias@gmail.com>
2018-09-23 16:19:01 -0300

    Implementation on java

--- app/src/main/java/com/mdias/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/mdias/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -2,8 +2,15 @@ package com.mdias.aluraviagens.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
 
 import com.mdias.aluraviagens.R;
+import com.mdias.aluraviagens.dao.PacoteDAO;
+import com.mdias.aluraviagens.model.Pacote;
+import com.mdias.aluraviagens.ui.adapter.ListaPacotesAdapter;
+
+import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -11,5 +18,11 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        ListView viewListaPacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        viewListaPacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
